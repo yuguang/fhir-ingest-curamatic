c17bb1a0a0954b0103b6a1d606e9777e842e3de4
yuguang <yz@yuguangzhang.com>
2024-03-27 13:20:54 -0700

    update requirements path

--- setup-pyenv.sh
+++ setup-pyenv.sh
@@ -1,4 +1,4 @@
 export PYTHON_VERSION=3.10
 pyenv virtualenv $PYTHON_VERSION "curamatic-fhir-ingest"
 pyenv activate "curamatic-fhir-ingest"
-pip install -r requirements.txt
+pip install -r ./src/requirements.txt
